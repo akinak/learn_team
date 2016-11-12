e60f56f6ef84c2b0c2cd2a821afdc4f9c17a6e86
Arevshatyanc <akinakii@yandex.ru>
2016-11-12 11:53:50 +0200

    first commit

---
+++ Class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akinak1
+ * Date: 2016-11-12
+ * Time: 11:52
+ */
+
+
+
+class Main {
+    public $foo;
+}
+
+
+class Child extends Main{
+    public $money = 100000;
+}
